f0360db7534de111c3c677fe039ae6770c2d1b10
hwnnn <2020112023@dgu.ac.kr>
2026-02-06 16:23:48 +0900

    chore: CORS 에러 수정(WebRTC 주소 추가)

--- src/main/java/com/example/clearcall/config/SecurityConfig.java
+++ src/main/java/com/example/clearcall/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("https://webrtc-smyn.onrender.com", "http://localhost:5173"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
